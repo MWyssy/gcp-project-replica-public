24c75f2e2841264217a55e943dba09be6ddd0a0b
MWyssy <mike@wyss.co.uk>
2023-06-27 21:17:35 +0100

    UPDATE: Small tweak to variables

--- _remote-state/variables.tf
+++ _remote-state/variables.tf
@@ -1,29 +1,35 @@
 variable "project_region" {
   description = "Region the project will sit in"
   type        = string
+  default     = "europe-west2"
 }
 
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default     = "gcp-project-replica-391115"
 }
 
 variable "project_zone" {
   description = "The region's zone the project will sit in"
   type        = string
+  default     = "europe-west2-a"
 }
 
 variable "keyring_name" {
   description = "Name of the keyring used for the remote state bucket"
   type        = string
+  default     = "gcp-pr-tfstate-keyring"
 }
 
 variable "key_name" {
   description = "Name of the key used for the remote state bucket"
   type        = string
+  default     = "gcp-pr-tfstate-key"
 }
 
 variable "bucket_name" {
   description = "Name of the remote state bucket"
   type        = string
+  default     = "gcp-project-replica-tfstate"
 }
