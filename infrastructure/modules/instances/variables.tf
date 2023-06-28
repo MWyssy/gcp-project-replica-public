2e69363770ce6e85d9c4ddf7fb5b00ce95783235
MWyssy <mike@wyss.co.uk>
2023-06-28 09:48:19 +0100

    UPDATE: Amend file structure

---
+++ infrastructure/modules/instances/variables.tf
@@ -0,0 +1,9 @@
+variable "fe_bucket_name" {
+  description = "Name of the frontend bucket"
+  type        = string
+}
+
+variable "project_region" {
+  description = "Region the project will sit in"
+  type        = string
+}
